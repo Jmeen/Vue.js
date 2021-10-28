7f5f239dc9a77e6c402103a5892fa2998f3661e4
Jaemin Lim <jmeeen@gmail.com>
2021-10-28 22:09:52 +0900

    emit

---
+++ test/src/components/inputField.vue
@@ -0,0 +1,31 @@
+<template>
+    <form action="">
+      <label for="">name</label>
+      <input type="text" 
+      v-model="name" 
+      style="padding:30px; border:2px solid green;"
+      @input="$emit('input', $event.target.value)"
+      />
+
+      <button type="submit" >submit</button>
+    </form>
+</template>
+
+<script>
+export default {
+    props:{
+        value : {
+            type:String,
+            require:true
+        }  
+    } ,
+    // methods:{
+        // updateName(e){
+        //         console.log(e.target.value)
+        //         // this.$emit("update-name",e.target.value)
+        // }
+    // }
+    
+
+}
+</script>
--- test/src/views/Home.vue
+++ test/src/views/Home.vue
@@ -1,49 +1,52 @@
 <template>
   <div>
     <h1>This is homepage</h1>
-        <p>{{name}}</p><br>
+  <InputField v-model="name" />
         <button @click="updateName">click</button>
+        <p>{{name}}</p>
   </div>
 </template>
 
 <script>
+import InputField from "@/components/inputField.vue";
 // import JMCoder from "@/components/jmView.vue";
 export default {
-  // components:{
-  //     JMCoder
-  //   },
+  components:{
+      // JMCoder
+      InputField
+    },
   data(){
     return{
-      name:'jaemin'
+      name:''
     }
   },
-  beforeCreate(){
-    console.log('beforeCreate',this.name)
-  },
-  created(){
-    console.log('Create',this.name)
-  },
-  beforeMount(){
-    alert("beforeMount")
-  },
-  mounted(){
-    alert("mount")
-  },
-  beforeUpdate(){
-    alert("beforeUpdate",this.name)
-  },
-  updated(){
-    alert("updated!@!!!")
-  },
-  beforeDestroy(){
-    alert('beforeDestroy');
-  },
-  destroyed(){
-    alert('destroyed')
-  },
+  // beforeCreate(){
+  //   console.log('beforeCreate',this.name)
+  // },
+  // created(){
+  //   console.log('Create',this.name)
+  // },
+  // beforeMount(){
+  //   alert("beforeMount")
+  // },
+  // mounted(){
+  //   alert("mount")
+  // },
+  // beforeUpdate(){
+  //   alert("beforeUpdate",this.name)
+  // },
+  // updated(){
+  //   alert("updated!@!!!")
+  // },
+  // beforeDestroy(){
+  //   alert('beforeDestroy');
+  // },
+  // destroyed(){
+  //   alert('destroyed')
+  // },
   methods:{
-    updateName(){
-      this.name="hello"
+    updateName(name){
+      this.name=name;
     }
   }
 }
