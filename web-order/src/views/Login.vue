e0131087e32965e0a1d8278089ea7d61ff81a89e
Jaemin Lim <jmeeen@gmail.com>
2021-11-08 16:03:55 +0900

    create login page and fuction

---
+++ web-order/src/views/Login.vue
@@ -0,0 +1,92 @@
+<template>
+<v-container fill-height style="max-width:350px;">
+
+    <v-layout align-center row wrap>
+
+        <v-flex xs12>
+                <v-alert
+                :value="iserror"
+                 type="error">
+                아이디와 비밀번호를 확인해주세요
+                </v-alert>
+                <v-alert
+                :value="issuccess"
+                 type="success">
+                로그인이 완료되었습니다.
+                </v-alert>
+           <v-card   elevation="2">
+                <v-toolbar  elevation="4" flat height="30">
+                    <v-toolbar-title>로그인</v-toolbar-title>
+                </v-toolbar>
+                <div class="pa-3">
+                    <v-text-field
+                        type="text"
+                        v-model="email"
+                        label="이메일을 입력하세요"
+                    >
+                    </v-text-field>
+                    <v-text-field
+                        type="password"
+                        v-model="password"
+                        label="패스워드를 입력하세요"
+                    >
+                    </v-text-field>
+                    <v-btn large block depressed @click="login()">Login</v-btn>
+                </div>
+
+            </v-card>
+        </v-flex>
+     
+  </v-layout>
+ 
+</v-container>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email:null,
+            password:null,
+            allusers:[
+                {id:'test',name:"jaemin",email:"test@test.com",password:"1234"},
+                {id:'test2',name:"jaemin2",email:"test2@test.com",password:"1234"},
+            ],
+            iserror:false,
+            issuccess:false
+        }
+    },
+    methods:{
+        login(){
+            // 전체 유저에서 해당 이메일로 유저 찾기
+            let selectedUser = null;
+            this.allusers.forEach(user=>{
+                if(user.email===this.email) selectedUser = user
+            })
+            selectedUser === null
+                ?(this.iserror=true, this.issuccess=false)
+                :selectedUser.password!==this.password
+                    ?(this.iserror=true, this.issuccess=false)
+                    :(this.issuccess=true, this.iserror=false)
+
+            // if(selectedUser === null) alert("이메일이 없습니다")
+            // else{
+            //     selectedUser.password!==this.password
+            //     ?alert("이메일과 비밀번호가 일치하지 않습니다")
+            //     :alert("로그인이 완료되었습니다.")
+                // if(selectedUser.password!==this.password) 
+                //     alert("이메일과 비밀번호가 일치하지 않습니다")
+                // else{
+                //     alert("로그인이 완료되었습니다.")
+                //     }
+            // }
+            // 그 유저의 비밀번호 비교
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
